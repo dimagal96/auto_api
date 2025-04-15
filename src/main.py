80a0f149e33bc6500431a9c7d3a1e51cfaa51e1a
dimagal96 <198794164+dimagal96@users.noreply.github.com>
2025-04-16 01:24:58 +0500

    feat: implement api

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from database import Database
 from data_types import *
 
@@ -17,12 +17,58 @@ database = Database(
 )
 
 @app.get('/cars')
+async def get_cars():
+    return database.get_cars()
+
+@app.get('/accidents')
+async def get_accidents():
+    return database.get_accidents()
+
+@app.get('/car')
 async def get_car(id: int):
     return {
         "id": id,
         "data": database.get_car(id)
     }
 
-@app.put('/cars')
+@app.put('/car')
 async def save_car(car: Car):
     return database.save_car(car)
+
+@app.delete('/car')
+async def delete_car(id: int):
+    return database.delete_car(id)
+
+@app.get('/accident')
+async def get_accident(id: int):
+    return {
+        "id": id,
+        "data": database.get_accident(id)
+    }
+
+@app.put('/accident')
+async def save_accident(accident: Accident):
+    return database.save_accident(accident)
+
+@app.delete('/accident')
+async def delete_accident(id: int):
+    return database.delete_accident(id)
+
+@app.get('/car/accidents')
+async def get_car_accidents(id: int):
+    return {
+        "id": id,
+        "data": database.get_car_accidents(id)
+    }
+
+@app.put('/car/accidents')
+async def save_car_accident(accident_car: AccidentCar):
+    return database.save_accident_car(accident_car)
+
+@app.delete('/car/accidents')
+async def delete_car_accident(id: int):
+    return database.delete_accident_car(id)
+
+@app.get('/types')
+async def get_types():
+    return database.get_types()
